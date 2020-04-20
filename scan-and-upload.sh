5fcd85970c9c8eacfecc27eb0171d03466fb3c8c
Robert Andruschow <robert.andruschow@googlemail.com>
2020-04-20 18:04:00 +0200

    fix ls command

--- scan-and-upload.sh
+++ scan-and-upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 read -p "directory: " directory_to_put_it
 ./scan.sh
-file_to_upload=$(ls *.jpg)
+file_to_upload=$(ls -t *.jpg | head -n 1)
 ./upload-to-gdrive.sh "$file_to_upload" "$directory_to_put_it"
 mkdir -p recently-scanned/
 mv "$file_to_upload" recently-scanned/
