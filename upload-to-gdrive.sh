67226f93481c1f71d8049df18e3f16d4ba44b1b4
Robert Andruschow <robert.andruschow@googlemail.com>
2020-05-10 06:58:51 +0200

    switch to rclone

--- upload-to-gdrive.sh
+++ upload-to-gdrive.sh
@@ -4,8 +4,7 @@ into_directory=$2
 set -e
 
 echo "Downloading scanned.7z"
-scanned_7z_id=$(./gdrive-linux-x64 --service-account gdrive-token.json list  | grep scanned.7z | head -n 1 | awk '{print $1}')
-./gdrive-linux-x64 --service-account gdrive-token.json download $scanned_7z_id --force
+rclone --verbose copy gdrive:/scanned.7z scanned.7z
 
 echo "Uploading '$file_to_upload' into scanned.7z folder '$into_directory'..."
 # add file to archive
@@ -14,6 +13,6 @@ echo "Uploading '$file_to_upload' into scanned.7z folder '$into_directory'..."
 7z rn scanned.7z "$file_to_upload" "scanned/${into_directory}/${file_to_upload}" -p"$(cat password)"
 
 # uploading scanned
-./gdrive-linux-x64 --service-account gdrive-token.json upload scanned.7z
+rclone --verbose move scanned.7z gdrive:/scanned.7z
 
 echo "Done."
